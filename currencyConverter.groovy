82f9ecd7085f5c62866229c64f9ee08894b93e47
Amy Ann Franz <amy.franz@gmail.com>
2022-10-18 18:31:06 +0100

    diff

--- currencyConverter.groovy
+++ currencyConverter.groovy
@@ -1,3 +1,4 @@
+//This is a comment
 print "If I give you £1, how many Euro will you give me? ";
 double euroOverPoundRatio = Double.parseDouble(System.console().readLine());
 print "If I give you 1€, how many Sterling pounds will you give me? ";
